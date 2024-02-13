3b7851d9bc885052c61a8982ef86d2b62b271b22
Ross <ross.h.sanderson@gmail.com>
2024-02-13 19:28:35 +1100

    results showing and on sub-pages

---
+++ src/lib/components/ResultsPage.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { media } from '$lib/api';
+	import type { MovieListResult } from '$lib/types';
+
+	export let movies: MovieListResult[];
+	export let next: string | null;
+</script>
+
+<div class="results">
+	{#each movies as movie}
+		<a href="/movies/{movie.id}">
+			<img alt={movie.title} src={media(movie.poster_path, 500)} />
+		</a>
+	{/each}
+</div>
+
+<style>
+	.results {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		width: 100%;
+		gap: 1em;
+	}
+
+	a {
+		width: 100%;
+		aspect-ratio: 2/3;
+	}
+
+	img {
+		width: 100%;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
+	import Carousel from '$lib/components/Carousel.svelte';
+	import Hero from './Hero.svelte';
+	import { views } from '$lib/views';
+
 	export let data;
+
 	console.log(data);
 </script>
 
-<h1>Top trending movies</h1>
+<h1 class="column">Top trending movies</h1>
+
+<Hero movie={data.featured} />
+
+<Carousel movies={data.trending.results} view={views.trending} href="/movies/trending" />
+<Carousel movies={data.now_playing.results} view={views.now_playing} href="/movies/now_playing" />
+<Carousel movies={data.upcoming.results} view={views.upcoming} href="/movies/upcoming" />
